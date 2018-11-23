52af6ac86227841029acfc79bdfa706730552fae
NickMeepo <383062105@qq.com>
2018-11-23 18:34:30 +0800

    initial

---
+++ PhotoAssetSaveSupported.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoAssetSaveSupported.swift
+//  Projects
+//
+//  Created by nickmeepo on 2018/11/22.
+//  Copyright © 2018年 nickmeepo. All rights reserved.
+//
+
+#if canImport(UIKit)
+import UIKit
+
+@objc public protocol PhotoAssetSaveSupported: AnyObject {
+    @objc func photoAssetSaved(image: UIImage?, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer?)
+}
+
+extension UIImage {
+    public func photoAssetSaved(target: PhotoAssetSaveSupported, context: UnsafeMutableRawPointer?) {
+        UIImageWriteToSavedPhotosAlbum(self, target, #selector(target.photoAssetSaved(image:didFinishSavingWithError:contextInfo:)), context)
+    }
+}
+
+
+#endif
+
+
